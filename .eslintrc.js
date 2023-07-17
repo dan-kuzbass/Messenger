ec894c614061625dfee361ae920cb9a05ab33847
daniil_guzikov <daniil_guzikov@smartworld.team>
2023-07-17 21:02:01 +0700

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,57 @@
 module.exports = {
+  env: {
+    node: true,
+  },
   root: true,
-  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
+  parserOptions: {
+    ecmaVersion: 2020, // enable parsing latest ECMAScript
+    sourceType: 'module', // allow use of imports
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'google',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
   ],
-};
+  rules: {
+    semi: 'off',
+    indent: 'off',
+    'max-len': ['off', { ignoreTemplateLiterals: true, ignoreStrings: true }],
+    'quote-props': 'off',
+    'operator-linebreak': 'off',
+    'object-curly-spacing': 'off',
+    'prettier/prettier': 'off',
+    camelcase: 'off',
+    'require-jsdoc': [
+      'error',
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: false,
+          ClassDeclaration: true,
+          ArrowFunctionExpression: false,
+          FunctionExpression: false,
+        },
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          String: false,
+          Boolean: false,
+          Number: false,
+          Symbol: false,
+          '{}': false,
+          Object: false,
+          object: false,
+          Function: false,
+        },
+        extendDefaults: true,
+      },
+    ],
+  },
+}
